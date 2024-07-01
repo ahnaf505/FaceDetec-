e2f0b28cff20142b10c12938a057147383a526a7
ahnaf505 <159417805+ahnaf505@users.noreply.github.com>
2024-07-01 14:04:10 +0000

    update standrd.html template fix annoying newline print

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from colorama import *
 import colorama
+import base64
+from PIL import Image
+from colorama import *
+from report import *
 from compare import *
 
 colorama.init(autoreset=True)
@@ -7,8 +10,28 @@ just_fix_windows_console()
 print(colorama.ansi.clear_screen())
 print(Back.BLACK + Fore.BLUE + "FaceDetec' | CLI Interface")
 print(Back.WHITE + "Press enter to start making a face comparison report...")
-input()
+input('\n')
 
+print('NOTE: THIS APP ONLY SUPPORTS JPG FORMAT')
 img1_pth = input("Please input path for the first face image: ")
 img2_pth = input("Please input path for the second face image: ")
 
+compare_output = run_compare(img1_pth, img2_pth)
+
+if compare_output == "err1":
+    print("No face detected or other error has occoured")
+    exit
+else:
+    pass
+
+
+img1_base64 = ""
+img2_base64 = ""
+with open(img1_pth, "rb") as f:
+    img1_base64 = base64.b64encode(f.read())
+    f.close()
+with open(img2_pth, "rb") as f:
+    img2_base64 = base64.b64encode(f.read())
+    f.close()
+
+generate_report(img1_base64, img2_base64, "face1", "face2")
--- report.py
+++ report.py
@@ -3,15 +3,17 @@
 report_standard_cache = ""
 
 def generate_report(img1_base64, img2_base64, img1_name, img2_name):
-    with open('/report_templates/standard.html', 'r') as file:
+    with open('report_templates/standard.html', 'r') as file:
         report_standard_cache = file.read()
         file.close()
 
     with open('face_comparison_report.html', 'w+') as file:
-        report_standard_cache = report_standard_cache.replace("##base64_img1##", img1_base64)
-        report_standard_cache = report_standard_cache.replace("##base64_img2##", img2_base64)
-        report_standard_cache = report_standard_cache.replace("##img1_name##", img1_name)
-        report_standard_cache = report_standard_cache.replace("##img2_name##", img2_name)
+        img1_base64 = str(img1_base64).rstrip("'")
+        img2_base64 = str(img2_base64).rstrip("'")
+        report_standard_cache = report_standard_cache.replace("##base64_img1##", img1_base64.replace("b'", ""))
+        report_standard_cache = report_standard_cache.replace("##base64_img2##", img2_base64.replace("b'", ""))
+        report_standard_cache = report_standard_cache.replace("##img1_name##", str(img1_name))
+        report_standard_cache = report_standard_cache.replace("##img2_name##", str(img2_name))
         file.write(report_standard_cache)
         file.close()
     
