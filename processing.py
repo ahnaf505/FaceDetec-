cddf13b89b067ed8539df6152c5c4d1cef595c3f
ahnaf505 <159417805+ahnaf505@users.noreply.github.com>
2024-07-06 06:45:52 +0000

    html report is 100% complete

--- processing.py
+++ processing.py
@@ -3,7 +3,6 @@ import cv2
 import base64
 import mediapipe as mp
 import numpy as np
-import mediapipe as mp
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5)
